4fc5f8af7ef93ac8f15b61c6a87319163dab9c39
B4JD1K <bajdik@yahoo.com>
2025-05-23 04:39:37 +0200

    added navbar redirecting to key pages and added it to the application

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import './App.css';
 import Home from "./pages/Home.jsx";
 import {Route, Routes} from "react-router-dom";
 import Favorites from "./pages/Favorites.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
 
     return (
-        <>
+        <div>
+            <NavBar/>
             <main className="main-content">
                 <Routes>
                     <Route path="/" element={<Home />}/>
                     <Route path="/favorites" element={<Favorites />}/>
                 </Routes>
             </main>
-        </>
+        </div>
     );
 }
 
